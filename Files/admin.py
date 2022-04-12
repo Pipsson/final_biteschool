aad14fa7d561e3c3ee60bf8b8f771bffb5af9e54
Pipsson <juliusmallya8420@gmail.com>
2022-04-12 10:21:11 +0300

    changes on urls and routes

--- Files/admin.py
+++ Files/admin.py
@@ -1,3 +1,22 @@
+
+
+# Register your models here.
 from django.contrib import admin
 
 # Register your models here.
+from .models  import Advance_math5, Form,Advanced_math1,Advanced_math2,Physics1,Physics2,Chemistry1,Chemistry2,Chemistry_form5,Chemistry_form6,Advance_math5,Advanced_math6,physics_form5,physics_form6,ChemistryPracticals,physics_practicals
+admin.site.register(Advanced_math1)
+admin.site.register(Advanced_math2)
+admin.site.register(Physics1)
+admin.site.register(Physics2)
+admin.site.register(Chemistry1)
+admin.site.register(Chemistry2)
+admin.site.register(Advance_math5)
+admin.site.register(Advanced_math6)
+admin.site.register(physics_form5)
+admin.site.register(physics_form6)
+admin.site.register(Chemistry_form5)
+admin.site.register(Chemistry_form6)
+admin.site.register(ChemistryPracticals)
+admin.site.register(physics_practicals)
+admin.site.register(Form)
--- biteschool/urls.py
+++ biteschool/urls.py
@@ -13,9 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from biteschool.settings import DEBUG , MEDIA_ROOT
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
+from Files import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('' ,views.homepage, name='home_return'),
+    path('about/' ,views.aboutpage, name='about_return'),
+    path('contact/' ,views.contactpage, name='contact_return'),
+    path('paper/' ,views.paperpage, name='paper_return'),
+    path('website/' ,views.webpage, name='web_return'),
+    path('book/' ,views.bookpage, name='book_return'),
+    path('book1/' ,views.book1page, name='book1_return'),
+    path('Notes/' ,views.Notespage, name='Notes_return'),    
+    path('practicals/' ,views.practicalspage, name='practicals_return'),     
+    re_path(r'^download/(?P<path>.*)$',serve,{'document_root' :settings.MEDIA_ROOT}),
+]                                  
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    
